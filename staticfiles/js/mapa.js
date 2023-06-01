8fed073c125a167ca90d95cb911f850f2343f52e
andreportol <andreportol@gmail.com>
2023-06-01 08:19:57 -0400

    Aplicado o comando collectstatic para criação do arquivo staticfiles

---
+++ staticfiles/js/mapa.js
@@ -0,0 +1,23 @@
+function initMap() {
+    // Obter coordenadas do celular usando o JavaScript
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
+
+        // Exibir as coordenadas no HTML
+        document.getElementById('latitude').textContent = lat.toFixed(6);
+        document.getElementById('longitude').textContent = lng.toFixed(6);
+
+        // Criar mapa usando as coordenadas obtidas
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: lat, lng: lng},
+            zoom: 16
+        });
+
+        // Marcar a localização no mapa
+        var marker = new google.maps.Marker({
+            position: {lat: lat, lng: lng},
+            map: map
+        });
+    });
+}
